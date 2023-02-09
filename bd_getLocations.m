dc3b11c9fadc0fb35b4c207908ce9e482dc2b363
Julie-Fabre <julie.m.fabre@gmail.com>
2023-02-09 18:27:03 +0000

    new atlas location

--- bd_getLocations.m
+++ bd_getLocations.m
@@ -4,5 +4,9 @@ atlasLocation = dir([brainglobeLocation, atlasType, '_',...
     atlasSpecies, '_', num2str(atlasResolution_um), 'um*']); % atlas location
 imgToRegister = dir([brainsawPath, '/*/', animal, '/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
 imgToTransform = dir([brainsawPath, '/*/', animal, '/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
+if isempty(imgToRegister) || contains(imgToRegister.folder, 'Recycle')
+    imgToRegister = dir(['/home/netshare/zaru/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
+    imgToTransform = dir(['/home/netshare/zaru/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
+end
 outputDir = [imgToRegister.folder, filesep, 'brainReg'];
 end
