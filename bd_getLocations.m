909b633cdabc8b153b3af07cff27948c7932df98
Julie-Fabre <julie.m.fabre@gmail.com>
2023-11-10 16:29:33 +0000

    clean up example script

--- bd_getLocations.m
+++ bd_getLocations.m
@@ -1,29 +1,14 @@
-function [atlasLocation, imgToRegister, imgToTransform, outputDir] =...
-    bd_getLocations(brainglobeLocation, brainsawPath, animal, channelColToRegister, channelColToTransform, atlasType, atlasSpecies, atlasResolution_um)
-atlasLocation = dir([brainglobeLocation, atlasType, '_',...
-    atlasSpecies, '_', num2str(atlasResolution_um), 'um*']); % atlas location
-% default in subjects 
-imgToRegister = dir(['/home/netshare/zaru/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
-imgToTransform = dir(['/home/netshare/zaru/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
+function [atlasLocation, imgToRegister, imgToTransform, outputDir] = ...
+    bd_getLocations(brainglobeLocation, brainsawPath, animal, channelColToRegister, channelColToTransform,...
+    atlasType, atlasSpecies, atlasResolution_um)
 
 
-if isempty(imgToRegister) 
-    imgToRegister = dir(['/home/netshare/zinu/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
-    imgToTransform = dir(['/home/netshare/zinu/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
-end
-if isempty(imgToRegister) 
-    imgToRegister = dir(['/home/netshare/znas/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
-    imgToTransform = dir(['/home/netshare/znas/', animal, '/*istology/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
-end
-% otherwise in znas-brainsaw-display warning 
-if isempty(imgToRegister) || contains(imgToRegister(1).folder, 'Recycle')
-    imgToRegister = dir([brainsawPath, '/*/', animal, '/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
-    imgToTransform = dir([brainsawPath, '/*/', animal, '/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
-end
-if isempty(imgToRegister)
-    imgToRegister = dir([brainsawPath, animal, '/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
-    imgToTransform = dir([brainsawPath, animal, '/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
-end
+atlasLocation = dir([brainglobeLocation, atlasType, '_', ...
+atlasSpecies, '_', num2str(atlasResolution_um), 'um*']); % atlas location
+
+imgToRegister = dir([brainsawPath, '/*/', animal, '/downsampled_stacks/025_micron/*', channelColToRegister, '*.tif*']);
+imgToTransform = dir([brainsawPath, '/*/', animal, '/downsampled_stacks/025_micron/*', channelColToTransform, '*.tif*']);
+
 imgToTransform = imgToTransform(1);
 imgToRegister = imgToRegister(1);
 outputDir = [imgToRegister(1).folder, filesep, 'brainReg'];
