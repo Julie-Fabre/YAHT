12766fff6abbacc96895aba4c262768b5a67ac9b
Julie-Fabre <julie.m.fabre@gmail.com>
2024-03-14 20:36:54 +0000

    convert allen/brainreg coords to common bregma/lambda ones (paxinos-like)

--- ya_convert_allen_to_paxinos.m
+++ ya_convert_allen_to_paxinos.m
@@ -4,33 +4,40 @@ function coordinates_bregma_mm = ya_convert_allen_to_paxinos(values_toConvert, a
 
 % (scaling "Toronto MRI transform", reflect AP/ML, convert 25/10um to 1mm)
 if strcmp(allenOrBrainreg, 'allen')
-    scale = [0.952, -1.031, 0.885] ./ 100; % [ML, AP, DV]
+    scale = [0.952, 1.031, 0.885] ./ 100; % [ML, AP, DV]
     bregma = [570, 540, 0]./100; % best estimate, [ML, AP, DV]
 elseif  strcmp(allenOrBrainreg, 'brainreg')
-    scale = [0.952, -1.031, 0.885] ./ 250; % [ML, AP, DV]
+    scale = [0.952, 1.031, 0.885] ./ 250; % [ML, AP, DV]
     bregma = [228, 216, 0]./ 250; % best estimate, [ML, AP, DV]
 else
     error('unrecognized value format')
 end
 
-% Scale the coordinates
-AP_scaled = values_toConvert(2) * scale(2);
-ML_scaled = values_toConvert(1) * scale(1);
-DV_scaled = values_toConvert(3) * scale(3);
+% Translation transformation matrix
+ccf_translation_tform = eye(4) + [zeros(3,4); -bregma, 0];
 
-% Tilt angle
-theta_degrees = 15; % Tilt by 15 degrees - this is my best approximation
-theta_radians = deg2rad(theta_degrees);
+% Scaling transformation matrix
+ccf_scale_tform = eye(4) .* [scale, 1]';
 
-% Rotation matrix for tilting around ML axis
-R = [cos(theta_radians) 0 sin(theta_radians); 0 1 0; -sin(theta_radians) 0 cos(theta_radians)];
+% Rotation (tilt) adjustment
+ap_rotation = 15; % Tilting the CCF 15 degrees nose-up - this is my best approximation
 
-% Apply rotation to the scaled AP and DV (ignoring ML in rotation)
-coordinates_scaled = [ML_scaled; AP_scaled; DV_scaled];
-coordinates_rotated_scaled = R * coordinates_scaled;
+% Rotation transformation matrix
+ccf_rotation_tform = ...
+    [1 0 0 0; ...
+     0 cosd(ap_rotation) -sind(ap_rotation) 0; ...
+     0 sind(ap_rotation) cosd(ap_rotation) 0; ...
+     0 0 0 1];
+
+% Combine transformations: Note the order of multiplication
+ccf_bregma_tform_matrix = ccf_translation_tform * ccf_scale_tform * ccf_rotation_tform;
+
+% If using the transformation matrix directly in MATLAB, e.g., with affine3d
+ccf_bregma_tform = affine3d(ccf_bregma_tform_matrix);
 
 % Adjusting values relative to bregma
-coordinates_bregma_mm = coordinates_rotated_scaled'  - bregma;
+coordinates_bregma_mm = transformPointsForward(ccf_bregma_tform, values_toConvert);
+
 
 
 end
